5686d02feb3008a17109239accdf726b39a2021c
bengbengku <anggiatbenget70@gmail.com>
2020-10-09 18:01:31 +0700

    ADD: Menyelesaikan styling sidebar #1

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import './Sidebar.css'
+import {Avatar} from "@material-ui/core";
+import SidebarChannel from "./SidebarChannel";
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import AddIcon from '@material-ui/icons/Add';
 import SignalCellularAltIcon from '@material-ui/icons/SignalCellularAlt';
 import CallIcon from '@material-ui/icons/Call';
 import InfoOutlineIcon from '@material-ui/icons/InfoOutlined';
-import SidebarChannel from "./SidebarChannel";
-import './Sidebar.css'
+import MicIcon from '@material-ui/icons/Mic';
+import HeadsetIcon from '@material-ui/icons/Headset';
+import SettingsIcon from '@material-ui/icons/Settings';
 
 function Sidebar() {
     return (
@@ -46,7 +50,20 @@ function Sidebar() {
                     <InfoOutlineIcon/>
                     <CallIcon />
                 </div>
-            </div>          
+            </div>   
+
+            <div className="sidebar__profile">
+                <Avatar src="https://avatars0.githubusercontent.com/u/42617505?s=460&u=1b725f9f405fe7e5a0f77ab112326619a8ac1d96&v=4"/>
+                <div className="sidebar__profileInfo">
+                    <h3>@bengbengku</h3>
+                    <p>#thisIsMyId</p>
+                </div>
+                <div className="sidebar__profileIcons">
+                    <MicIcon />
+                    <HeadsetIcon />
+                    <SettingsIcon />
+                </div>
+            </div>
         </div>
     )
 }
