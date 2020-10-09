876cfdc45a379042f44e46394a4c5103f8b70d8c
bengbengku <anggiatbenget70@gmail.com>
2020-10-09 19:01:29 +0700

    ADD: Styling chatHeader menggunakan flexbox

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Sidebar from './Sidebar';
+import Chat from './Chat';
 import './App.css';
 
 function App() {
   return (
     <div className="app">
       <Sidebar />
+      <Chat />
     </div>
   );
 }
---
+++ src/ChatHeader.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import "./ChatHeader.css";
+import NotificationsIcon from '@material-ui/icons/Notifications';
+import EditLocationRoundedIcon from '@material-ui/icons/EditLocationRounded';
+import PeopleAltRoundedIcon from '@material-ui/icons/PeopleAltRounded';
+import SearchIcon from '@material-ui/icons/Search';
+import SendIcon from '@material-ui/icons/Send';
+import HelpIcon from '@material-ui/icons/Help';
+
+function ChatHeader() {
+    return (
+        <div className="chatHeader">
+            <div className="chatHeader__left">
+                <h3>
+                    <span className="chatHeader__hash">
+                        #
+                    </span>
+                    Youtube
+                </h3>
+            </div>
+
+            <div className="chatHeader__right">
+                <NotificationsIcon />
+                <EditLocationRoundedIcon />
+                <PeopleAltRoundedIcon />
+
+                <div className="chatHeader__search">
+                    <input placeholder="Search"/>
+                    <SearchIcon />
+                </div>
+
+                <SendIcon />
+                <HelpIcon />
+            </div>
+        </div>
+    )
+}
+
+export default ChatHeader;
