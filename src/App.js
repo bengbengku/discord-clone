4533a051eb914aa54643ea9b63cc49d3f108878c
bengbengku <anggiatbenget70@gmail.com>
2020-10-09 21:04:46 +0700

    ADD: Beri akses jika user true redirect ke home

--- src/App.js
+++ src/App.js
@@ -3,14 +3,22 @@ import Sidebar from './Sidebar';
 import Chat from './Chat';
 import './App.css';
 import { useSelector } from 'react-redux';
-
+import { selectUser } from "./features/userSlice";
 
 function App() {
-  const user = useSelector()
+  const user = useSelector(selectUser);
+  
   return (
     <div className="app">
-      <Sidebar />
-      <Chat />
+      {user ? (
+        <>
+        <Sidebar />
+        <Chat />
+        </>
+      ) : (
+        <h2>Anda harus login</h2>
+      )}
+      
     </div>
   );
 }
